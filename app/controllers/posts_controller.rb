b58631920d7b92ca78433fbd7036fd7cfa02bc0b
Joon <kudo@Joons-MacBook-Air.local>
2014-11-28 13:44:06 -0700

    blog post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  def index
+   @posts = Post.all
+ end
+
+ def show
+   @post = Post.find(params[:id])
+ end
+
+ def new
+   @post = Post.new
+ end
+
+ def create
+  @post = Post.new(post_params)
+
+  if @post.save
+    redirect_to posts_path
+  else
+    render :new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if 
+      @post.update_attributes(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :author, :body)
+end
+end
